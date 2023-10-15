4367ed57cb3c22a6e20d792d925266d84ae318aa
Aymen <34170109+Riae@users.noreply.github.com>
2023-10-15 21:44:35 +0200

    intial

---
+++ src/components/molecules/Socials.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Social from '../atoms/Social.svelte';
+	import { user } from '../../util/discord';
+</script>
+
+<div class="socials-container">
+	<Social icon="github" tip="Riae" link="https://github.com/riae" />
+	<Social icon="discord" tip={user.username} link={`https://discord.com/users/${user.id}`} />
+	<Social icon="kofi" tip="Tristanaa" link="https://ko-fi.com/tristanaa" />
+</div>
+
+<style>
+	.socials-container {
+		display: flex;
+		gap: 0.75rem;
+	}
+</style>
