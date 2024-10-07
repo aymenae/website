ce037187f98aafdee89286bbf77ab724903ab7e4
aymenae <aymenebrahim15@gmail.com>
2024-10-07 15:16:37 +0200

    Closed on mondays

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,32 @@
 	import Supporters from '../components/organisms/Supporters.svelte';
 	import Footer from '../components/molecules/Footer.svelte';
 	import Experiences from '../components/organisms/Experiences.svelte';
+	import Monday from '../components/organisms/Monday.svelte';
+	import { toZonedTime } from 'date-fns-tz';
+	import { onMount } from 'svelte';
+
+	let isMonday = false;
+
+	onMount(() => {
+		// Only run this code on the client
+		const amsterdamTimeZone = 'Europe/Amsterdam';
+		const currentDate = new Date();
+		const zonedDate = toZonedTime(currentDate, amsterdamTimeZone);
+
+		// Check if today is Monday (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+		isMonday = zonedDate.getDay() === 1;
+	});
 </script>
 
-<NavHost />
-<main>
-	<Hero />
-	<About />
-	<Repos />
-	<Experiences />
-	<Supporters />
-	<Footer />
-</main>
+{#if isMonday}
+	<Monday />
+{:else}
+	<main>
+		<Hero />
+		<About />
+		<Repos />
+		<Experiences />
+		<Supporters />
+		<Footer />
+	</main>
+{/if}
