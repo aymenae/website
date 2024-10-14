7daca039fd067e6cc765f38d176a2ad62639f770
aymenae <aymenebrahim15@gmail.com>
2024-10-14 15:03:42 +0200

    whitespace add

---
+++ src/components/atoms/Whitespace.svelte
@@ -0,0 +1,11 @@
+<script>
+	// No props or logic needed for this component
+</script>
+
+<div class="whitespace"></div>
+
+<style lang="scss">
+	.whitespace {
+		height: 65vh; /* 85% of the viewport height */
+	}
+</style>
